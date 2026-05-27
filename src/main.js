6820c0d9b8898e3c2a1fee95938d54580764c61f
MAN <157442416+MANXOAN@users.noreply.github.com>
2026-05-27 16:31:13 +0700

    Restore full quiz app as main entry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppPractical.jsx';
+import App from './App.jsx';
 import './style.css';
 
 const mountNode = globalThis.document && globalThis.document.getElementById('root');
